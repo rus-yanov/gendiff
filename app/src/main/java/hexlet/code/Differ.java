a0bb3499d27363704a7f82857f51408e6260ffc8
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-02-15 17:40:44 +0300

    add jacoco & update differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -33,24 +33,8 @@ public class Differ {
         return Formatter.getOutput(differ, format);
     }
 
-    // generate result (default stylish format)
-
-    public static String generate(String filePath1,
-                                  String filePath2)
-            throws Exception {
-
-        String content1 = Utils.getContent(filePath1);
-        String content2 = Utils.getContent(filePath2);
-
-        String extension1 = Utils.getFileExtension(filePath1);
-        String extension2 = Utils.getFileExtension(filePath2);
-
-        Map<String, Object> data1 = Parse.parseContent(content1, extension1);
-        Map<String, Object> data2 = Parse.parseContent(content2, extension2);
-
-        Map<String, Item> differ = getDiff(data1, data2);
-
-        return Formatter.makeStylish(differ);
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1, filePath2, "stylish");
     }
 
     // getting difference between parsed data
